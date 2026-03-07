83820c8a58acb412ee181fac751034ac66c04686
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-07 21:53:21 +0100

    Mise en place des routes React Router

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Error() {
+  return (
+    <div>
+      <h1>Erreur 404 : Page introuvable</h1>
+    </div>
+  );
+}
+
+export default Error;
---
+++ src/pages/Logement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logement() {
+  return (
+    <div>
+      <h1>Fiche Logement</h1>
+    </div>
+  );
+}
+
+export default Logement;
