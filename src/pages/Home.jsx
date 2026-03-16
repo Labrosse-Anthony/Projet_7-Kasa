18e676bf45956ba6a58ce7679a5f955332ce0e16
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-16 08:13:50 +0100

    Création de banner et ajout sur la page d'acceuil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../components/banner';
 
 function Home() {
   return (
     <div>
-      <h1>Page d'Accueil</h1>
+      <Banner />
     </div>
   );
 }
