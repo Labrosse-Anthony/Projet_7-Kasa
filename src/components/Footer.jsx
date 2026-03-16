9f9a7d3c678447da556ecc85da1a59402a0b9877
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-16 17:37:14 +0100

    Creation fichir jsx et style du footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import logoWhite from '../assets/images/logo-white.svg'
+import '../assets/style/footer.scss'
 
 function Footer() {
   return (
-    <footer>
+    <footer className='footer'>
+      <img src= {logoWhite} alt="logo kasa blanc"/>
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   );
