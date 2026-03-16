6020ca566eb16a68b3bafbff0ae99b131a12fd8a
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-16 08:41:59 +0100

    Mise en page Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.scss'
+import logo from '../assets/images/logo-red.svg';
 
 function Header() {
   return (
-    <header>
-      <img src="logo-kasa.png" alt="Logo Kasa" />
+    <header className="header">
+      <img src={logo} alt="Logo Kasa" />
       <nav>
         <Link to="/">Accueil</Link>
         <Link to="/about">A Propos</Link>
