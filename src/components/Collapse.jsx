3ea5cd7838f1d48c500eba5b1208c84bf8db29fd
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-18 14:48:22 +0100

    Creation composant Collapse

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import arrow from '../assets/images/arrow.svg';
+import '../assets/style/collapse.scss';
+
+function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse">
+      <div className="collapse-header" onClick={toggleCollapse}>
+        <h3>{title}</h3>
+        <img 
+          src={arrow} 
+          alt="Ouvrir/Fermer" 
+          className={isOpen ? "arrow arrow-up" : "arrow arrow-down"} 
+        />
+      </div>
+      
+      {isOpen && (
+        <div className="collapse-content">
+          <p>{content}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Collapse;
