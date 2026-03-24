28ae9a1d972d35a6c949cde244b74267ae8f8cda
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-24 08:24:25 +0100

    Creation structure de la page erreur

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../pages/error.scss';
 
 function Error() {
   return (
-    <div>
-      <h1>Erreur 404 : Page introuvable</h1>
-    </div>
+    <main className="error-page">
+      <h1 className="error-code">404</h1>
+      <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
+      
+      {/* Le composant Link permet de naviguer sans recharger la page */}
+      <Link to="/" className="error-link">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
   );
 }
 
