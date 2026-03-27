637f5bc5328639dd4e48bc813198dd1babf014ce
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-27 17:40:25 +0100

    Creation de la page de Logement.jsx

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useParams, Navigate } from 'react-router-dom';
+/* Vérifie bien le chemin vers ton fichier JSON (adapte-le si besoin) */
+import data from '../assets/data/Logements.json'; 
+import Slideshow from '../components/Slideshow';
 
 function Logement() {
+  /* On récupère l'ID directement depuis l'URL (ex: /logement/c67ab8a7) */
+  const { id } = useParams();
+  /* On cherche dans notre base de données (le JSON) l'appartement qui a le même ID */
+  const logement = data.find((item) => item.id === id);
+
+  if (!logement) {
+    return <Navigate to="/404" />;
+  }
+
   return (
-    <div>
-      <h1>Fiche Logement</h1>
-    </div>
+    <main className="logement-page">
+      <Slideshow pictures={logement.pictures} />
+      
+    </main>
   );
 }
 
