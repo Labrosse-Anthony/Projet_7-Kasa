79036f33be997ad1cd0b05015248a2e18560aacd
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-03-30 18:03:47 +0200

    Ajout du soulignement actif sur le menu de navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './header.scss'
+import { NavLink } from 'react-router-dom';
+import './header.scss';
 import logo from '../assets/images/logo-red.svg';
 
 function Header() {
@@ -8,8 +8,8 @@ function Header() {
     <header className="header">
       <img src={logo} alt="Logo Kasa" />
       <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A Propos</Link>
+        <NavLink to="/" end>Accueil</NavLink>
+        <NavLink to="/about">A Propos</NavLink>
       </nav>
     </header>
   );
